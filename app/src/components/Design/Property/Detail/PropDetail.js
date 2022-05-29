d015578880363e2ac0feceeb8350913496b6d575
Andries-DB <andrdeba@student.arteveldehs.be>
2022-05-29 19:11:13 +0200

    Property Detail

--- app/src/components/Design/Property/Detail/PropDetail.js
+++ app/src/components/Design/Property/Detail/PropDetail.js
@@ -2,47 +2,61 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import millify from 'millify';
 import { FaBed, FaBath } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
 import { BsGridFill } from 'react-icons/bs';
 import useFetch from '../../../../core/hooks/useFetch';
 import imageOne from '../../Public/MainSection/ImageSlider/data/images/house-1.jpg';
 import Button from '../../Button/Button';
 import '../Property.css';
 
+import useTitle from '../../../../core/hooks/useTitle';
+
 function PropDetail() {
   const { id } = useParams();
 
   const { data: property } = useFetch(`/immo/${id}`);
-  console.log(property);
+  const { t } = useTranslation();
+  useTitle(property?.title);
   return (
-    <div className="cardDetail">
-      <div className="cardDetail__body">
-        <img src={imageOne} alt={property?.title} className="cardDetail__img" />
-        <h2 className="cardDetail__title">{property?.title}</h2>
-        <div className="cardDetail__Rooms">
-          {property?.amountBedrooms}
-          <FaBed />
-          {' '}
-          |
-          {' '}
-          {property?.amountBathrooms}
-          <FaBath />
-          |
-          {' '}
-          {property?.size}
-          <p>
-            m
-            <sup>2</sup>
-          </p>
-          <BsGridFill />
+    <div className="app">
+      <div className="details">
+        <div className="big-img">
+          <img src={imageOne} alt={property?.title} />
         </div>
-        <h4 className="cardDetail__adress">{property?.adress}</h4>
-        <Button
-          color="primary"
-          type="button"
-          className="cardDetail__btn"
-        >
-          {`${property?.type} this!`}
-        </Button>
+        <div className="box">
+          <div className="row">
+            <h2>{property?.title}</h2>
+            <span>
+              €
+              {property?.price}
+            </span>
+          </div>
+          <div className="cardRooms">
+            {property?.amountBedrooms}
+            <FaBed />
+            {' '}
+            |
+            {' '}
+            {property?.amountBathrooms}
+            <FaBath />
+            |
+            {'  '}
+            {property?.size}
+            <p>
+              m
+              <sup>2</sup>
+            </p>
+            <BsGridFill />
+          </div>
+          <Button
+            color="primary"
+            type="button"
+            className="btn"
+          >
+            {`${property?.type} this!`}
+          </Button>
+        </div>
+
       </div>
     </div>
 
